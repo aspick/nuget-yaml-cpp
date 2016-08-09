c8ddfa0888a8e71724881887a9079d4bee7daffb
Yugo <yugo@18th-lab.com>
2016-08-09 14:28:12 +0900

    update

--- compose.ps1
+++ compose.ps1
@@ -4,8 +4,11 @@ mkdir nuget\build\native\lib
 cp yaml-cpp\build\Debug\* nuget\build\native\lib\
 cp yaml-cpp\build\Release\* nuget\build\native\lib\
 
+cd nuget
+# download nuget.exe
+Invoke-WebRequest -Uri http://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile .\
+
 # update meta
 
 # packing
-cd nuget
 .\nuget pack yaml-cpp.nuspec
